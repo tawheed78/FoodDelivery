aa324ab311191d885d9d2c5c1c2d50bdc4e73930
tawheed78 <tawheed.chilwan55@gmail.com>
2023-01-11 14:48:02 +0530

    implemented html templates for email

--- orders/views.py
+++ orders/views.py
@@ -6,10 +6,11 @@ from marketplace.context_processors import get_cart_amounts
 from menu.models import FoodItem
 from .forms import OrderForm
 from .models import Order, OrderedFood, Payment
-from .utils import generate_order_number
+from .utils import generate_order_number, order_total_by_vendor
 from account.utils import send_notification
 from django.contrib.auth.decorators import login_required
 
+from django.contrib.sites.shortcuts import get_current_site
 
 
 @login_required(login_url='login')
@@ -132,10 +133,21 @@ def payments(request):
         # SEND ORDER CONFIRMATION EMAIL TO THE CUSTOMER
         mail_subject = 'Thank you for ordering with us.'
         mail_template = 'orders/order_confirmation_email.html'
+
+        ordered_food = OrderedFood.objects.filter(order=order)
+        customer_subtotal = 0
+        for item in ordered_food:
+            customer_subtotal += (item.price * item.quantity)
+        tax_data = json.loads(order.tax_data)
+
         context = {
             'user': request.user,
             'order': order,
             'to_email': order.email,
+            'ordered_food':ordered_food,
+            'domain': get_current_site(request),
+            'customer_subtotal': customer_subtotal,
+            'tax_data':tax_data,
         }
         send_notification(mail_subject, mail_template, context)
         
@@ -147,11 +159,17 @@ def payments(request):
         for i in cart_items:
             if i.fooditem.vendor.user.email not in to_emails:
                 to_emails.append(i.fooditem.vendor.user.email)
-        context = {
-            'order':order,
-            'to_email':to_emails,
-        }
-        send_notification(mail_subject, mail_template, context)
+
+                ordered_food_to_vendor = OrderedFood.objects.filter(order=order, fooditem__vendor=i.fooditem.vendor)
+                context = {
+                    'order':order,
+                    'to_email':i.fooditem.vendor.user.email,
+                    'ordered_food_to_vendor':ordered_food_to_vendor,
+                    'vendor_subtotal':order_total_by_vendor(order, i.fooditem.vendor.id)['subtotal'],
+                    'tax_data':order_total_by_vendor(order, i.fooditem.vendor.id)['tax_dict'],
+                    'vendor_grand_total': order_total_by_vendor(order, i.fooditem.vendor.id)['grand_total']
+                }
+                send_notification(mail_subject, mail_template, context)
         
 
 
